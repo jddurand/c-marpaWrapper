f7af4703e1566100d2f41d86df572b246079b07a
Jean-Damien Durand <jeandamiendurand@free.fr>
2022-07-30 10:12:50 +0200

    Update after marpa_codes.h protection

--- amalgamation/marpaWrapper.c
+++ amalgamation/marpaWrapper.c
@@ -1,10 +1,6 @@
-#ifndef MARPAWRAPPER_AMALGAMATION
-#define MARPAWRAPPER_AMALGAMATION
-
 #include "../libmarpa/work/stage/marpa_ami.c"
 #include "../libmarpa/work/stage/marpa_avl.c"
 #include "../libmarpa/work/stage/marpa.c"
-/* marpa_codes includes marpa_codes.h that is not protected - c.f. the usage of MARPAWRAPPER_AMALGAMATION in internal/_logging.h */
 #include "../libmarpa/work/stage/marpa_codes.c"
 #include "../libmarpa/work/stage/marpa_obs.c"
 #include "../libmarpa/work/stage/marpa_tavl.c"
@@ -12,5 +8,3 @@
 #include "../src/grammar.c"
 #include "../src/recognizer.c"
 #include "../src/value.c"
-
-#endif /* MARPAWRAPPER_AMALGAMATION */
