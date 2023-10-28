1d0ab85f62d7641af21418c6e9b7896b7a0a0147
Jean-Damien Durand <jeandamiendurand@free.fr>
2023-10-28 12:48:25 +0200

    Squashed '3rdparty/github/genericSparseArray/' changes from 97724e99e..75559b80c
    75559b80c Version 1.0.52
    8dcfc3bf5 Fix workflows and remove STATIC for genericLogger test dependency
    a95ad8e51 Merge commit '326fd45f6bf92b5d7528128d6ce2c692f2fdcb7b'
    326fd45f6 Squashed '3rdparty/github/genericLogger/' changes from 219b7d6..fbaa4a0
    40a39a76d Merge commit '9fcb0a83614b8f8cf43d6e2788363ad9cd219e4d'
    9fcb0a836 Squashed '3rdparty/github/genericHash/' changes from 98c03bd..d0ec102
    82a053fa6 Merge commit '62480b1b4be2d757db1319f4962409e339b046ff'
    62480b1b4 Squashed '3rdparty/github/cmake-utils/' changes from f6b67a7..d397ea1
    6cb1ca752 Version 1.0.51
    1282de321 Squashed '3rdparty/github/genericLogger/' changes from 767bd1c..219b7d6
    0de851acc Merge commit '1282de321ba15500ba915c8e3f1066499752eb62'
    ed4304f71 Squashed '3rdparty/github/genericHash/' changes from 6fbbd95..98c03bd
    b6dce1e4f Merge commit 'ed4304f714aae2ddd725a899f7442e1ac156770c'
    a0a755a47 Merge commit '61668cd5923f66835cb10a02df7468927ec1ac29'
    61668cd59 Squashed '3rdparty/github/cmake-utils/' changes from 4387a0b..f6b67a7
    
    git-subtree-dir: 3rdparty/github/genericSparseArray
    git-subtree-split: 75559b80c959b0020c8a0096fa40f1cf00659767

--- 3rdparty/github/cmake-utils/cmake/MyPackageCmakeExport.cmake
+++ 3rdparty/github/cmake-utils/cmake/MyPackageCmakeExport.cmake
@@ -1,5 +1,5 @@
 MACRO (MYPACKAGECMAKEEXPORT)
-  IF (NOT MYPACKAGECMAKEEXPORT_${PROJECT_NAME}_DONE)
+  IF ((NOT ${PROJECT_NAME}_NO_CONFIGEXPORT) AND (NOT MYPACKAGECMAKEEXPORT_${PROJECT_NAME}_DONE))
     SET (MYPACKAGECMAKEEXPORT_${PROJECT_NAME}_DONE TRUE)
 
     IF (NOT CMAKE_VERSION VERSION_LESS "3.26")
@@ -19,12 +19,43 @@ MACRO (MYPACKAGECMAKEEXPORT)
       #
       # Explicit public dependencies
       #
-      FOREACH (_public_dependency ${${PROJECT_NAME}_public_dependencies})
-        STRING (APPEND _target_cmake_in "find_package(${_public_dependency} REQUIRED)")
+      STRING (APPEND _target_cmake_in "set(${PROJECT_NAME}_PACKAGE_DEPENDENCIES \"${${PROJECT_NAME}_package_dependencies}\")")
+      STRING (APPEND _target_cmake_in [[
+
+]])
+      IF (${PROJECT_NAME}_package_dependencies)
+        STRING (APPEND _target_cmake_in "include(CMakeFindDependencyMacro)")
         STRING (APPEND _target_cmake_in [[
 
 ]])
-      ENDFOREACH ()
+        FOREACH (_package_dependency ${${PROJECT_NAME}_package_dependencies})
+          GET_PROPERTY(_package_dependency_version GLOBAL PROPERTY MYPACKAGE_DEPENDENCY_${_package_dependency}_VERSION)
+          GET_PROPERTY(_package_dependency_version_major GLOBAL PROPERTY MYPACKAGE_DEPENDENCY_${_package_dependency}_VERSION_MAJOR)
+          GET_PROPERTY(_package_dependency_version_minor GLOBAL PROPERTY MYPACKAGE_DEPENDENCY_${_package_dependency}_VERSION_MINOR)
+          STRING (APPEND _target_cmake_in [[
+
+]])
+          STRING (APPEND _target_cmake_in "set(${PROJECT_NAME}_PACKAGE_DEPENDENCY_${_package_dependency}_VERSION ${_package_dependency_version})")
+          STRING (APPEND _target_cmake_in [[
+
+]])
+          STRING (APPEND _target_cmake_in "set(${PROJECT_NAME}_PACKAGE_DEPENDENCY_${_package_dependency}_VERSION_MAJOR ${_package_dependency_version_major})")
+          STRING (APPEND _target_cmake_in [[
+
+]])
+          STRING (APPEND _target_cmake_in "set(${PROJECT_NAME}_PACKAGE_DEPENDENCY_${_package_dependency}_VERSION_MINOR ${_package_dependency_version_minor})")
+          STRING (APPEND _target_cmake_in [[
+
+]])
+          STRING (APPEND _target_cmake_in "find_dependency(${_package_dependency} \${${PROJECT_NAME}_PACKAGE_DEPENDENCY_${_package_dependency}_VERSION})")
+          STRING (APPEND _target_cmake_in [[
+
+]])
+        ENDFOREACH ()
+      ENDIF ()
+      STRING (APPEND _target_cmake_in [[
+
+]])
       STRING (APPEND _target_cmake_in "include(\"\${CMAKE_CURRENT_LIST_DIR}/${_export_targets}.cmake\"\)")
       STRING (APPEND _target_cmake_in [[
 
@@ -36,7 +67,7 @@ MACRO (MYPACKAGECMAKEEXPORT)
         COMPONENT LibraryComponent)
       INCLUDE (CMakePackageConfigHelpers)
       IF (MYPACKAGE_DEBUG)
-        MESSAGE (STATUS "[${PROJECT_NAME}-START-DEBUG] Generating ${PROJECT_NAME}Config.cmake")
+        MESSAGE (STATUS "[${PROJECT_NAME}-CMAKEEXPORT-DEBUG] Generating ${PROJECT_NAME}Config.cmake")
       ENDIF ()
       CONFIGURE_PACKAGE_CONFIG_FILE(${TARGET_CMAKE_IN}
         ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake
@@ -45,12 +76,12 @@ MACRO (MYPACKAGECMAKEEXPORT)
         NO_CHECK_REQUIRED_COMPONENTS_MACRO
       )
       IF (MYPACKAGE_DEBUG)
-        MESSAGE (STATUS "[${PROJECT_NAME}-START-DEBUG] Generating ${PROJECT_NAME}ConfigVersion.cmake")
+        MESSAGE (STATUS "[${PROJECT_NAME}-CMAKEEXPORT-DEBUG] Generating ${PROJECT_NAME}ConfigVersion.cmake")
       ENDIF ()
       WRITE_BASIC_PACKAGE_VERSION_FILE (
         ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake
-        VERSION "${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}"
-        COMPATIBILITY SameMinorVersion
+        VERSION ${PROJECT_VERSION}
+        COMPATIBILITY SameMajorVersion
       )
       INSTALL (FILES
         ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake
